ab4891ed7d9e7373aefd7ed52523e50537ad82cf
Pakseh <dasbyu6@gmail.com>
2026-07-22 17:53:32 +0700

    Update db.js

--- assets/js/db.js
+++ assets/js/db.js
@@ -1,8 +1,24 @@
 const db = new Dexie("KasirKuDB");
 
+// Versi lama
 db.version(1).stores({
 
     barang: "++id,nama,kategori,harga,stok",
+
+    transaksi: "++id,tanggal,total,tunai,kembalian",
+
+    detail: "++id,transaksiId,barangId",
+
+    setting: "id"
+
+});
+
+// Versi baru (menambah tabel kategori)
+db.version(2).stores({
+
+    barang: "++id,nama,kategori,harga,stok",
+
+    kategori: "++id,nama",
 
     transaksi: "++id,tanggal,total,tunai,kembalian",
 
